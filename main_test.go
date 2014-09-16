251c88822e426bc49942ac45421cf46a1f7b74f2
Stefan Arentz <stefan@arentz.ca>
2014-09-15 21:14:56 -0400

    Adding some more unit tests for better coverage

--- main_test.go
+++ main_test.go
@@ -31,3 +31,36 @@ func Test_handleAssertion(t *testing.T) {
 		t.Fatalf("Non-expected status code%v:\n\tbody: %v", "200", response.Code)
 	}
 }
+
+func Test_handleAssertionWithUniqueClientKey(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/assertion?email=test@mockmyid.com&audience=http://localhost&uniqueClientKey=true", nil)
+	response := httptest.NewRecorder()
+
+	handleAssertion(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Non-expected status code%v:\n\tbody: %v", "200", response.Code)
+	}
+}
+
+func Test_handleAssertionWithMissingEmail(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/assertion?audience=http://localhost", nil)
+	response := httptest.NewRecorder()
+
+	handleAssertion(response, request)
+
+	if response.Code != http.StatusBadRequest {
+		t.Fatalf("Non-expected status code%v:\n\tbody: %v", http.StatusBadRequest, response.Code)
+	}
+}
+
+func Test_handleAssertionWithMissingAudience(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/assertion?email=test@mockmyid.com", nil)
+	response := httptest.NewRecorder()
+
+	handleAssertion(response, request)
+
+	if response.Code != http.StatusBadRequest {
+		t.Fatalf("Non-expected status code%v:\n\tbody: %v", http.StatusBadRequest, response.Code)
+	}
+}
